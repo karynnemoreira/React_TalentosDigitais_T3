91eda033cc6c70e80b54e9a29571143dbe9e273f
karynnemoreira <karynnemoreira@yahoo.com.br>
2024-01-24 19:48:41 -0300

    primeira aula react t3

---
+++ primeira-aula-t3/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import  "./App.css"
+import Header from "./Header"
+
+function App() {
+  
+  return (
+    <>
+    <Header />
+      <h1>Olá T3</h1>
+    </>
+  )
+}
+
+export default App
---
+++ primeira-aula-t3/src/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react"; 
+
+function Header(){
+    return(
+<nav>
+    <ul> 
+        <li> Início</li>
+        <li>Sobre</li>
+        <li>Contatos</li>
+    </ul>
+</nav>
+    )
+}
+export default Header
